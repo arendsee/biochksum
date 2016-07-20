ecfce6ef879aa2c82915c1bdf15d5bfd5f971069
Zebulun Arendsee <arendsee@iastate.edu>
2016-07-20 00:50:34 -0500

    First hack (broken)

---
+++ biochksum.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 8
+
+void print_chksum(unsigned char chksum[]){
+    for(int i = 0; i < SIZE; i++){
+        printf("%x ", chksum[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char * argv[]){
+    FILE * fasta = fopen("sample-data/proteins.faa", "r");
+
+    unsigned char chksum[] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    size_t pos = 0;
+    unsigned char c = 0;
+
+    while((read = getline(&line, &len, fasta)) != EOF){
+        
+        if(line[0] == '>'){
+            pos = 0;
+            continue;
+        }
+        for(size_t i = 0; i < len; i++){
+            c = line[i];
+            if(c >= 'A' && c <= 'Z'){
+                c += 32;
+            }
+            if(c >= 'a' && c <= 'z'){
+                c = (c - 97) * (256.0 / 26);
+                for(size_t j = 0; j < 8; j++){
+                    j = (j + pos) % 8;
+                    chksum[j] += ((128 >> j) & c);
+                }
+                /* print_chksum(chksum); */
+                pos++;
+            }
+        }
+    }
+    free(line);
+    fclose(fasta);
+
+
+    return EXIT_SUCCESS;
+}
